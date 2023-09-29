40234e68b60d020c69c7f2baea067c2a9453a030
Rachael Asiwe <rachaelisioma69@gmail.com>
2023-09-29 16:04:24 +0100

    added work flow

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command})=>{
-  const config = {
-    base: "/",
-    plugins: [react()],
-    server: {
-      port: 3000,
-    }
-  }
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
-  return config
+// export default defineConfig(({command})=>{
+//   const config = {
+//     base: "/",
+//     plugins: [react()],
+//     server: {
+//       port: 3000,
+//     }
+//   }
+//   if (command !== 'serve') {
+//     config.base = '/react-vite-gh-pages/'
+//   }
+//   return config
+// })
+
+export default defaultConfig({
+  plugins: [react()],
+  base:"/ray-bank-app/"
 })
