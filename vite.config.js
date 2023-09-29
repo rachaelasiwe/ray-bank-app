9f259bd98c92275207938d5bf30607dbe4b58553
Rachael Asiwe <rachaelisioma69@gmail.com>
2023-09-29 15:39:00 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:"./",
-  plugins: [react()],
-  server: {
-    port: 3000,
+export default defineConfig(({command})=>{
+  const config = {
+    base: "/",
+    plugins: [react()],
+    server: {
+      port: 3000,
+    }
   }
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+  return config
 })
